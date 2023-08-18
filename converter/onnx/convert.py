cb91fef057491967f0ec71dbf1c86d90dfb459d6
Jeanne <25933880+fleuryj@users.noreply.github.com>
2023-08-18 15:04:01 +0200

    Add ONNX converter for ultralytics

--- converter/onnx/convert.py
+++ converter/onnx/convert.py
@@ -4,16 +4,12 @@ from helpers.constants import DEFAULT_MODEL_OUTPUT_DIR, DEFAULT_INPUT_RESOLUTION
     DEFAULT_QUANTIZATION_TYPE, DEFAULT_ONNX_NAME, DEFAULT_IOU_THRESHOLD, DEFAULT_CONF_THRESHOLD, \
     DEFAULT_MAX_NUMBER_DETECTION
 from onnx_converter.pytorch_to_onnx_converter import PytorchToONNXConverter
-
+from tf_utils.parameters import ModelParameters
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('--model', type=str, dest="model_input_path", required=True,
                         help=f"The path to yolov5 model.")
-    parser.add_argument('--out', type=str, dest="model_output_directory", default=DEFAULT_MODEL_OUTPUT_DIR,
-                        help=f"The path to the directory in which to save the converted model. Default: {DEFAULT_MODEL_OUTPUT_DIR}")
-    parser.add_argument('--output-name', type=str, dest="model_output_name",
-                        default=DEFAULT_ONNX_NAME, help=f'The model output name. Default: {DEFAULT_ONNX_NAME}')
     parser.add_argument('--input-resolution', type=int, dest="input_resolution", default=DEFAULT_INPUT_RESOLUTION,
                         help=f'The resolution of the input images, e.g. {DEFAULT_INPUT_RESOLUTION} means input resolution is {DEFAULT_INPUT_RESOLUTION}x{DEFAULT_INPUT_RESOLUTION}. Default: {DEFAULT_INPUT_RESOLUTION}')  # height, width
     parser.add_argument('--quantize-model', nargs='+', dest="quantization_types", default=[DEFAULT_QUANTIZATION_TYPE],
@@ -24,14 +20,18 @@ def main():
                         help=f"The IoU threshold, if set in the model. Default: {DEFAULT_IOU_THRESHOLD}")
     parser.add_argument('--conf-threshold', type=float, default=DEFAULT_CONF_THRESHOLD,
                         help=f"The confidence threshold, if set in the model. Default: {DEFAULT_CONF_THRESHOLD}")
+    parser.add_argument('--no-nms', action='store_true', help='If set, NMS is not added at the end of the model.')
+    parser.add_argument('--no-normalization', action='store_true',
+                        help='If set, normalization is not added at the beginning of the model.')
+
     opt = parser.parse_args()
 
+    model_parameters = ModelParameters(input_resolution=opt.input_resolution, include_nms=not opt.no_nms,
+                                       include_normalization=not opt.no_normalization, max_det=opt.max_det)
+
     converter = PytorchToONNXConverter(model_input_path=opt.model_input_path,
-                                       model_output_directory=opt.model_output_directory,
-                                       model_output_name=opt.model_output_name,
-                                       input_resolution=opt.input_resolution,
+                                       model_parameters=model_parameters,
                                        quantization_types=opt.quantization_types,
-                                       max_det=opt.max_det,
                                        iou_threshold=opt.iou_threshold,
                                        conf_threshold=opt.conf_threshold)
 
--- converter/onnx/onnx_converter/pytorch_to_onnx_converter.py
+++ converter/onnx/onnx_converter/pytorch_to_onnx_converter.py
@@ -1,44 +1,70 @@
 from pathlib import Path
-
+import logging
 import onnx
 import onnxruntime
 import torch
-from onnxruntime.quantization.quantize import quantize_dynamic, QuantType
-from onnxruntime.transformers.float16 import convert_float_to_float16
-
 from helpers.constants import OUTPUT_DIR, DEFAULT_ONNX_NAME, DEFAULT_INPUT_RESOLUTION, \
     DEFAULT_QUANTIZATION_TYPE, ONNX_SUFFIX, END_COLOR, BLUE, GREEN, RED, FLOAT32, INT8, FLOAT16, FLOAT32_SUFFIX, \
     FLOAT16_SUFFIX, INT8_SUFFIX, OPSET, BATCH_SIZE, NB_CHANNEL, DEFAULT_IOU_THRESHOLD, DEFAULT_CONF_THRESHOLD, \
-    BOUNDINGBOX_NAME, CLASSES_NAME, SCORES_NAME, IMAGE_NAME, DEFAULT_MAX_NUMBER_DETECTION, \
-    PREDICTIONS_NAME
+    BOUNDINGBOX_NAME, CLASSES_NAME, SCORES_NAME, DEFAULT_MAX_NUMBER_DETECTION, \
+    PREDICTIONS_NAME, IMAGE_NAME, SEGMENTATION, MASKS_NAME, PT_SUFFIX, DETECTION, PROTOS_NAME
+from onnxruntime.quantization.quantize import quantize_dynamic, QuantType
+from onnxruntime.transformers.float16 import convert_float_to_float16
 from pytorch_utils.pytorch_loader import PyTorchModelLoader
 from pytorch_utils.pytorch_nms import YoloNMS
+from tf_utils.parameters import ModelParameters
 
 
 class PytorchToONNXConverter:
+    """ Class that converts a Pytorch model to a ONNX model
+
+        Attributes
+        ----------
+        model_input_path: String
+            The path to the Pytorch model
+
+        model_output_path: String
+            The path to the converted model
+
+        quantization_types: List[str]
+            The quantization types to use for the conversion
+
+        input_resolution: int
+            The input resolution of the image
+
+        include_nms: bool
+            Whether the converted model contains NMS
+
+        include_normalization: bool
+            Whether the converter model contains normalization
+
+        iou_threshold: float
+            The IoU threshold if the model contains NMS
+
+        conf_threshold: float
+            The confidence threshold if the model contains NMS
+
+        max_det: int
+            The maximum number of detections that can be made by the CoreML model
+        """
 
     def __init__(self, model_input_path,
-                 model_output_directory=OUTPUT_DIR,
-                 model_output_name=DEFAULT_ONNX_NAME,
-                 input_resolution=DEFAULT_INPUT_RESOLUTION,
-                 include_nms=True,
-                 include_normalization=True,
+                 model_output_path=None,
+                 model_parameters=ModelParameters(),
                  quantization_types=None,
                  iou_threshold=DEFAULT_IOU_THRESHOLD,
                  conf_threshold=DEFAULT_CONF_THRESHOLD,
                  max_det=DEFAULT_MAX_NUMBER_DETECTION):
 
         self.model_input_path = Path(model_input_path)
-        self.model_output_directory_path = Path(model_output_directory)
-        self.model_output_name = model_output_name
+        self.model_output_path = Path(model_output_path) if model_output_path else Path(
+            model_input_path.replace(PT_SUFFIX, ''))
 
         if quantization_types is None:
             quantization_types = [DEFAULT_QUANTIZATION_TYPE]
         self.quantization_types = quantization_types
 
-        self.input_resolution = input_resolution
-        self.include_nms = include_nms
-        self.include_normalization = include_normalization
+        self.model_parameters = model_parameters
 
         self.iou_threshold = iou_threshold
         self.conf_threshold = conf_threshold
@@ -51,36 +77,33 @@ class PytorchToONNXConverter:
         '''
         Converts a PyTorch model to a ONNX model
         '''
-        # Load the model
-        print(f'{BLUE}Loading PyTorch model...{END_COLOR}')
-        self.__check_input_create_output()
-        self.__init_pytorch_model()
 
         # Maybe add NMS layer and normalization
-        self.model = YoloNMS(self.model, max_det=self.max_det, iou_thres=self.iou_threshold,
-                             conf_thres=self.conf_threshold, normalized=self.include_normalization,
-                             nmsed=self.include_nms)
+        self.model = YoloNMS(self.pt_model, max_det=self.max_det, iou_thres=self.iou_threshold,
+                             conf_thres=self.conf_threshold, normalized=self.model_parameters.include_normalization,
+                             nmsed=self.model_parameters.include_nms)
 
-        file_name = self.__get_converted_model_name()
-        temp_converted_model_path = self.model_output_directory_path / file_name
+        temp_converted_model_path = self.__get_converted_model_name()
 
         print(f'{BLUE}Converting model to ONNX with onnx {onnx.__version__}...{END_COLOR}')
         try:
             # Create a first version
-            if self.include_nms:
-                torch.onnx.export(self.model,
-                                  self.sample_input,
-                                  temp_converted_model_path, verbose=False, opset_version=OPSET,
-                                  training=torch.onnx.TrainingMode.EVAL,
-                                  input_names=[IMAGE_NAME],
-                                  output_names=[BOUNDINGBOX_NAME, CLASSES_NAME, SCORES_NAME])
-            else:
-                torch.onnx.export(self.model,
-                                  self.sample_input,
-                                  temp_converted_model_path, verbose=False, opset_version=OPSET,
-                                  training=torch.onnx.TrainingMode.EVAL,
-                                  input_names=[IMAGE_NAME],
-                                  output_names=[PREDICTIONS_NAME])
+            inputs = [IMAGE_NAME]
+            outputs = [PREDICTIONS_NAME]
+            if self.model_parameters.model_type == SEGMENTATION:
+                outputs = [PREDICTIONS_NAME, PROTOS_NAME]
+            if self.model_parameters.include_nms:
+                if self.model_parameters.model_type == SEGMENTATION:
+                    outputs = [BOUNDINGBOX_NAME, CLASSES_NAME, SCORES_NAME, MASKS_NAME]
+                else:
+                    outputs = [BOUNDINGBOX_NAME, CLASSES_NAME, SCORES_NAME]
+
+            torch.onnx.export(self.model,
+                              self.sample_input,
+                              temp_converted_model_path, verbose=False, opset_version=OPSET,
+                              training=torch.onnx.TrainingMode.EVAL,
+                              input_names=inputs,
+                              output_names=outputs)
 
             # Checks
             model_onnx = onnx.load(temp_converted_model_path)  # load onnx model
@@ -107,55 +130,59 @@ class PytorchToONNXConverter:
                 else:
                     raise ValueError(f"The quantization type '{quantization_type}' is not supported.")
 
-                converted_model_path = self.model_output_directory_path / Path(
-                    file_name.replace(ONNX_SUFFIX, suffix + ONNX_SUFFIX))
+                converted_model_path = temp_converted_model_path.replace(ONNX_SUFFIX, suffix + ONNX_SUFFIX)
                 # Add metadata (labels)
-                self.__add_metadata(model_temp, self.model.names, quantization_type)
+                self.__add_metadata(model_temp, inputs, outputs, quantization_type)
 
                 onnx.save(model_temp, converted_model_path)
                 print(f'{GREEN}ONNX export success:{END_COLOR} saved as {converted_model_path}')
             except Exception as e:
-                temp_converted_model_path.unlink()
+                Path(temp_converted_model_path).unlink()
                 raise Exception(f'{RED}ONNX export failure:{END_COLOR} {e}')
 
-        temp_converted_model_path.unlink()
+        Path(temp_converted_model_path).unlink()
+
+    def __add_metadata(self, model, inputs, outputs, quantization_type):
+        metadata = self.model_parameters.metadata
+        metadata.update({'input_names': inputs,
+                         'output_names': outputs})
+        metadata.update({'quantization': quantization_type})
+
+        for k, v in metadata.items():
+            meta = model.metadata_props.add()
+            meta.key, meta.value = k, str(v)
 
     def __check_input_create_output(self):
         # Check input and create output
         if not self.model_input_path.exists():
             print(f"{RED}Model not found:{END_COLOR} '{str(self.model_input_path)}'")
             exit(0)
-        self.model_output_directory_path.mkdir(parents=True, exist_ok=True)
+        self.model_output_path.parent.mkdir(parents=True, exist_ok=True)
 
     def __init_pytorch_model(self):
-        self.model = PyTorchModelLoader(self.model_input_path, self.input_resolution).load(fuse=True)
-        self.sample_input = torch.zeros((BATCH_SIZE, NB_CHANNEL, self.input_resolution, self.input_resolution))
+        self.pt_model = PyTorchModelLoader(self.model_input_path, self.model_parameters).load(fuse=True)
+        self.sample_input = torch.zeros((BATCH_SIZE, NB_CHANNEL, self.model_parameters.input_resolution, self.model_parameters.input_resolution))
 
-    def __add_metadata(self, model, labels, quantization_type):
-        # Add metadata
-        meta = model.metadata_props.add()
-        meta.key = 'labels'
-        meta.value = ','.join(labels)
-
-        meta = model.metadata_props.add()
-        meta.key = 'quantized'
-        meta.value = quantization_type
-
-        meta = model.metadata_props.add()
-        meta.key = 'do_normalize'
-        meta.value = str(self.include_normalization)
-
-        meta = model.metadata_props.add()
-        meta.key = 'do_nms'
-        meta.value = str(self.include_nms)
+        logging.info(f"{BLUE}\t- The model has {self.model_parameters.nb_classes} classes{END_COLOR}")
+        logging.info(
+            f"{BLUE}\t- The model is for {self.model_parameters.model_type} ({self.model_parameters.model_orig}){END_COLOR}")
+        logging.info(
+            f"{BLUE}\t- Normalization will{'' if self.model_parameters.include_normalization else ' not'} be included in the model.{END_COLOR}")
+        logging.info(
+            f"{BLUE}\t- NMS will{'' if self.model_parameters.include_nms else ' not'} be included in the model.{END_COLOR}")
 
     def __get_converted_model_name(self):
-        basename = self.model_output_name
+        basename = str(self.model_output_path)
 
-        if not self.include_normalization:
+        if not self.model_parameters.include_normalization:
             basename += '_no-norm'
-        if not self.include_nms:
+        if not self.model_parameters.include_nms:
             basename += '_no-nms'
 
-        basename += f'_{self.input_resolution}'
+        basename += f'_{self.model_parameters.input_resolution}'
+
+        if self.model_parameters.model_type == DETECTION:
+            basename += "_detection"
+        else:
+            basename += "_segmentation"
         return basename + ONNX_SUFFIX
